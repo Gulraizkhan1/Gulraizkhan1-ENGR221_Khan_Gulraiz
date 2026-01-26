e09574df7687c07d0f548714860178236eff225a
Gulraizkhan1 <gkhan@sfsu.edu>
2026-01-26 14:37:08 -0800

    Lab 1 commit

--- Lab1.py
+++ Lab1.py
@@ -1,2 +1,2 @@
-message = "Go gators!" # This is a variable named 'message' that stores the string "Go gators!"
+message = "Go gators!" # This is a variable named 'message' that stores the string "Go gccators!"
 print(message) # This line prints the variable message 
