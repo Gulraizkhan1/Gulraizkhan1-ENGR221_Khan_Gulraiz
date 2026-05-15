e887a29af4040db33d67b2de91da212932504ae5
Gulraizkhan1 <gkhan@sfsu.edu>
2026-05-15 02:46:15 -0700

    Lab 8 commit

--- Lab8 (starter)/binarySearchTree.py
+++ Lab8 (starter)/binarySearchTree.py
@@ -1,3 +1,10 @@
+"""
+Author: Gulraiz Khan
+Filename: binarySearchTree.py
+Description: Implementation of a Binary Search Tree class
+Date: April 13, 2026
+"""
+
 class BinarySearchTree:
     """ An implementation of an unbalanced Binary Search Tree that stores key-value pairs. """
 
@@ -5,17 +12,33 @@ class BinarySearchTree:
         self.__root = None # The root Node of this BST
 
     def insert(self, insertKey, insertValue):
-        """ Inserts the given key and value into the BST. """
+        """ Inserts the given key and value into the BST.
+            Inputs:
+                - insertKey: (any) The key to insert
+                - insertValue: (any) The value to insert
+            Returns: None
+        """
+        # Update the root to include the inserted node                
         self.__root = self.__insertHelp(self.__root, insertKey, insertValue)
     
     def __insertHelp(self, node, insertKey, insertValue):
-        """ A recursive helper method to insert a new node into the BST. """
+        """ A recursive helper method to insert a new node 
+            with the given key and value into the BST.
+            Inputs:
+                - node: (Node) The root of the subtree to insert into
+                - insertKey: (any) The key to insert
+                - insertvalue: (any) The value to insert
+            Returns: The node to insert """
+        # Base case - Insert the node as a leaf in the appropriate location
         if node == None:
             return self.__Node(insertKey, insertValue)
+        # Insert the key into the left subtree if it is less than the current key
         elif insertKey < node.key:
             node.left = self.__insertHelp(node.left, insertKey, insertValue)
+        # Insert the key into the right subtree if it is greater than the current key
         elif insertKey > node.key:
             node.right = self.__insertHelp(node.right, insertKey, insertValue)
+        # Return the node with the node inserted
         return node
 
     def isEmpty(self):
@@ -73,18 +96,14 @@ class BinarySearchTree:
         elif deleteKey > node.key:
             node.right = self.__deleteHelp(node.right, deleteKey)
         else:
-            # Case 1 & 2: One child or no child
             if node.left is None:
                 return node.right
             elif node.right is None:
                 return node.left
             
-            # Case 3: Two children
-            # Get the in-order successor (smallest in the right subtree)
             successor = self.__findSuccessorHelp(node.right)
             node.key = successor.key
             node.value = successor.value
-            # Delete the successor
             node.right = self.__deleteHelp(node.right, successor.key)
 
         return node
@@ -101,27 +120,48 @@ class BinarySearchTree:
             self.__traverseHelp(node.right)
 
     def __str__(self) -> str:
+        """ Represent the tree as a string. Formats as 
+            {(rootkey, rootval), {leftsubtree}, {rightsubtree}} """
         return self.__strHelp("", self.__root)
     
     def __strHelp(self, return_string, node) -> str:
+        """ A recursive helper method to format the tree as a string. 
+            Input: 
+                - return_string: (string) Accumulates the final string to output
+                - node: (Node) The current node to format
+            Returns: A formatted string for this node. """
+        # Base case - Represent an empty branch as "None"
         if node == None:
             return "None"
+        # Recursively build the string to return
+        # Note, this is equivalent to
+        #   return "{" + node + ", " + \
+        #                self.strHelp(return_string, node.left) + ", " + \
+        #                self.strHelp(return_string, node.right) + "}"
         return "{{{}, {}, {}}}".format(node, 
                                        self.__strHelp(return_string, node.left), 
                                        self.__strHelp(return_string, node.right))
 
+    ##############
+    # NODE CLASS #
+    ##############
+
     class __Node:
+        """ Implementation of a node in a BST. Note that it is 
+            private, so it cannot be accessed outside of a BST """
+
         def __init__(self, key, value, left=None, right=None):
-            self.key = key
-            self.value = value
-            self.left = left
-            self.right = right
+            self.key = key         # The key of the root node of this tree
+            self.value = value     # The value held by the root node of this tree
+            self.left = left       # Points to the root of the left subtree
+            self.right = right     # Points to the root of the right subtree
 
         def __str__(self):
+            """ Represent the node as a string.
+                Formats as "{key, value}" """
             return "({}, {})".format(self.key, self.value)
         
 if __name__ == "__main__":
-    # Quick manual check
     bst = BinarySearchTree()
     bst.insert(50, "Root")
     bst.insert(30, "Left Child")
